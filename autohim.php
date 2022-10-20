896070e34fb903dc698de409b8f538525c0bd54e
Alexey Ageev <pochtovii.1996@mail.ru>
2022-10-21 01:40:48 +0300

    add: content in autohim.php

--- autohim.php
+++ autohim.php
@@ -5,6 +5,240 @@ $link_breadcrumb = 'autohim.php';
 $bread_last = 'last';
 include_once('header-page.php');?>
 
+<section class="section-contract-autohim">
+  <div class="container">
+    <div class="contract-autohim-content-wrapper">
+    <picture>
+      <source type="image/webp" srcset="img/contract-autohim.webp">
+      <source type="image/jpeg" srcset="img/contract-autohim.png">
+      <img  class="contract-autohim-mg" src="img/contract-autohim.png" alt="contract-autohim">
+    </picture>
+    <div class="contract-autohim-content">
+      <div class="seporator"></div>
+      <h2 class="section-title title-contract-autohim">Контрактное производство <br> автомобильной химии</h2>
+      <p class="contract-autohim-text">Прежде всего, начало повседневной работы по формированию позиции однозначно фиксирует необходимость приоритизации разума над эмоциями. Кстати, представители современных социальных резервов неоднозначны и будут описаны максимально подробно.</p>
+      <p class="contract-autohim-text">
+      Но социально-экономическое развитие напрямую зависит от поставленных обществом задач! Безусловно, постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для системы обучения кадров, соответствующей насущным потребностям. В целом, конечно, выбранный нами инновационный путь обеспечивает широкому кругу (специалистов) участие в формировании своевременного выполнения сверхзадачи. А ещё элементы политического процесса представлены в исключительно положительном свете. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высокотехнологичная концепция общественного уклада создаёт предпосылки для глубокомысленных рассуждений. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.
+      </p>
+    </div>  
+    </div>
+    <div class="swiper" id="contract-autohim">
+          <!-- обертка слайдера -->
+          <ul class="swiper-wrapper header-features header-features-our">
+            <!-- слайды -->
+            <li class="swiper-slide  header-features-item border-features">
+              <svg class="features-icon-1 features-mobile-icons">
+                <use href="img/sprite.svg#time"></use>
+              </svg>
+              <p class="header-features-text features-text-dark">Непрерывная работа c 2017 года</p>
+            </li>
+            <li class="swiper-slide  header-features-item border-features">
+              <svg class="features-icon-2 features-mobile-icons">
+                <use href="img/sprite.svg#cetificate"></use>
+              </svg>
+              <p class="header-features-text features-text-dark">Вся продукция сертифицирована</p>
+            </li>
+            <li class="swiper-slide  header-features-item border-features">
+              <svg class="features-icon-3 features-mobile-icons">
+                <use href="img/sprite.svg#control"></use>
+              </svg>
+              <p class="header-features-text features-text-dark">Контроль качества на всех этапах</p>
+            </li>
+            <li class="swiper-slide  header-features-item border-features">
+              <svg class="features-icon-4 features-mobile-icons">
+                <use href="img/sprite.svg#delivery"></use>
+              </svg>
+              <p class="header-features-text features-text-dark">Возможны поставки по всей России</p>
+            </li>
+            <li class="swiper-slide  header-features-item border-features">
+              <svg class="features-icon-5 features-mobile-icons">
+                <use href="img/sprite.svg#speed"></use>
+              </svg>
+              <p class="header-features-text features-text-dark">Оперативное производство</p>
+            </li>
+          </ul>
+         
+          <!-- кнопки навигации вперед/назад -->
+          <div class="sliders-buttons">
+            <div class="slider-button-prev">
+              <svg class="arrow-prev">
+                <use href="img/sprite.svg#arrow-prev"></use>
+              </svg>
+            </div>
+            <div class="slider-button-next">
+              <svg class="arrow-next">
+                <use href="img/sprite.svg#arrow-next"></use>
+              </svg>
+            </div> 
+          </div>
+          <!-- /.sliders-buttons -->
+    </div>  
+    
+  </div>
+      <!-- /.container -->
+</section>
+
+<section class="section-production-of-auto-chemicals">
+  <div class="container">
+    <div class="production-of-auto-chemicals-content-wrapper">
+      <div class="production-of-auto-chemicals-content">
+        <div class="seporator"></div>
+        <h2 class="section-title title-production-of-auto-chemicals">Изготовление автохимии</h2>
+        <p class="production-of-auto-chemicals-text">Но современная методология разработки позволяет оценить значение прогресса профессионального сообщества. Есть над чем задуматься: тщательные исследования конкурентов набирают популярность среди определенных слоев населения, а значит, должны быть ассоциативно распределены по отраслям. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.</p>
+        <ul class="clients-list our-client-list">   
+          <li class="clients-list-item him">
+            <svg class="client-logo">
+              <use href="img/sprite.svg#him"></use>
+            </svg>
+            Полное описание товаров
+          </li>
+          <li class="clients-list-item car">
+            <svg class="client-logo">
+              <use href="img/sprite.svg#car"></use>
+            </svg>
+            Актуальные цены
+          </li>
+          <li class="clients-list-item him">
+            <svg class="client-logo">
+              <use href="img/sprite.svg#him"></use>
+            </svg>
+            Полное описание товаров
+          </li>
+          <li class="clients-list-item car">
+            <svg class="client-logo">
+              <use href="img/sprite.svg#car"></use>
+            </svg>
+            Актуальные цены
+          </li>
+          <li class="clients-list-item brush">
+              <svg class="client-logo">
+                <use href="img/sprite.svg#brush"></use>
+              </svg>
+              Лаки и краски
+          </li>
+          <li class="clients-list-item him">
+            <svg class="client-logo">
+              <use href="img/sprite.svg#him"></use>
+            </svg>
+            Химические производства
+          </li>
+          <li class="clients-list-item brush">
+              <svg class="client-logo">
+                <use href="img/sprite.svg#brush"></use>
+              </svg>
+              Лаки и краски
+          </li>
+          <li class="clients-list-item him">
+            <svg class="client-logo">
+              <use href="img/sprite.svg#him"></use>
+            </svg>
+            Химические производства
+          </li>
+        </ul>
+      </div>
+      <picture>
+        <source type="image/webp" srcset="img/production-of-auto-chemicals.webp">
+        <source type="image/jpeg" srcset="production-of-auto-chemicals.png">
+        <img  class="production-of-auto-chemicals-mg" src="production-of-auto-chemicals.png" alt="production-of-auto-chemicals">
+      </picture> 
+    </div>
+</section>
+
+<section class="section-types-of-autochemistry">
+  <div class="container">
+    <div class="types-of-autochemistry-wrapper">
+      <div class="typers-of-autochemistry-content">
+        <div class="seporator"></div>
+        <h2 class="section-title title-ctypes-of-autochemistry">Виды автохимии</h2>
+        <p class="types-of-autochemistry-text">Но современная методология разработки позволяет оценить значение прогресса профессионального сообщества. Есть над чем задуматься: тщательные исследования конкурентов набирают популярность среди определенных слоев населения, а значит, должны быть ассоциативно распределены по отраслям.</p>
+      </div>
+      <ul class="clients-list typers-of-autochemistry-list">   
+          <li class="clients-list-item types-list him">
+            <div class="types"></div>
+            Антискотч
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Многоцелевая смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Жидкий ключ
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Высокотемпературная смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Универсальный очиститель деталей
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Силиконовая смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Цепная смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Медная смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Графитовая смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель кондиционера пенный
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель тормозных дисков
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель битумных пятен
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель кондционера
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель дросельной заслонки
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель обивки
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Тефлоновая смазка
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Очиститель двигателя
+          </li>
+          <li class="clients-list-item types-list him">
+            <div class="types"></div>
+            Универсальный очиститель
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Чернитель шин
+          </li>
+          <li class="clients-list-item  types-list him">
+            <div class="types"></div>
+            Чернитель шин + очистка
+          </li>
+        </ul>
+    </div>
+  </div>
+</section>
+
+<?php include_once('template-parts/aliance-production-block.php');?>
 
 <?php include_once('template-parts/steps-block.php');?>
 

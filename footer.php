f360cafe789461404ff4b6ffa961a1b3740943b7
Alexey Ageev <pochtovii.1996@mail.ru>
2022-10-21 03:46:12 +0300

    add: policy.php

--- footer.php
+++ footer.php
@@ -138,7 +138,7 @@
           <div class="footer-wrapper">
             <div class="footer-legal">
               <p class="footer-copyright">&copy; <?php echo date('Y');?> «Aliance Production». Все права защищены.</p>
-              <a href="#" class="footer-policy">Политики конфиденциальности</a>
+              <a href="policy.php" class="footer-policy">Политики конфиденциальности</a>
             </div>
             <div class="footer-author">
               <span class="made-in">Сделано в</span>
---
+++ policy.php
@@ -0,0 +1,145 @@
+<?php 
+$page_title = 'Политика конфиденциальноти';
+$link_breadcrumb = 'news.php';
+include_once('header-page.php');?>
+
+<section class="section-policy">
+  <div class="container">
+    <div class="policy-content">
+      <p class="policy-text">Внезапно, тщательные исследования конкурентов призывают нас к новым свершениям, которые, в свою очередь, должны быть объявлены нарушающими общечеловеческие нормы этики и морали. Безусловно, внедрение современных методик представляет собой интересный эксперимент проверки направлений прогрессивного развития! Как принято считать, элементы политического процесса формируют глобальную экономическую сеть и при этом - подвергнуты целой серии независимых исследований.
+      </p>
+
+      <p class="policy-text">Курс на социально-ориентированный национальный проект представляет собой интересный эксперимент проверки распределения внутренних резервов и ресурсов. Господа, экономическая повестка сегодняшнего дня не даёт нам иного выбора, кроме определения соответствующих условий активизации. Значимость этих проблем настолько очевидна, что постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для соответствующих условий активизации. Имеется спорная точка зрения, гласящая примерно следующее: тщательные исследования конкурентов могут быть обнародованы. Социально-экономическое развитие предопределяет высокую востребованность укрепления моральных ценностей.
+      </p>
+
+      <p class="policy-text">Безусловно, высокотехнологичная концепция общественного уклада позволяет оценить значение прогресса профессионального сообщества. Таким образом, глубокий уровень погружения требует определения и уточнения системы массового участия.
+      </p>
+
+      <p class="policy-text">Безусловно, экономическая повестка сегодняшнего дня обеспечивает актуальность существующих финансовых и административных условий. Как принято считать, некоторые особенности внутренней политики освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, указаны как претенденты на роль ключевых факторов. Базовые сценарии поведения пользователей будут рассмотрены исключительно в разрезе маркетинговых и финансовых предпосылок. Повседневная практика показывает, что высококачественный прототип будущего проекта играет определяющее значение для укрепления моральных ценностей. Ясность нашей позиции очевидна: базовый вектор развития обеспечивает широкому кругу (специалистов) участие в формировании инновационных методов управления процессами. Таким образом, реализация намеченных плановых заданий не даёт нам иного выбора, кроме определения переосмысления внешнеэкономических политик.
+      </p>
+
+      <p class="policy-text">Задача организации, в особенности же дальнейшее развитие различных форм деятельности предопределяет высокую востребованность экономической целесообразности принимаемых решений. Господа, перспективное планирование влечет за собой процесс внедрения и модернизации поставленных обществом задач. Кстати, независимые государства набирают популярность среди определенных слоев населения, а значит, должны быть своевременно верифицированы.
+      </p>
+    </div>
+  </div>
+</section>
+
+<footer class="footer">
+  <div class="container">
+        <div class="footer-top">
+          <a class="footer-logo" href="./">
+            <svg class="logo-svg footer-logo">
+              <use href="img/sprite.svg#logo"></use>
+            </svg>
+        </a>
+          <a href="tel:+74996861014" class="footer-phone">+7 (499) 686-10-14</a>
+          <div class="footer-info">
+            <svg class="phone-icon" width="24" height="24">
+              <use href="img/sprite.svg#mark"></use>
+            </svg>
+            <address class="footer-info-address">г. Мосвка, Холодильный пер. 4к1с8</address>
+          </div>
+          <!-- /.footer-info -->
+    
+          <div class="footer-info footer-info-2">
+            <svg class="phone-icon" width="24" height="24">
+              <use href="img/sprite.svg#mail"></use>
+            </svg>
+            <a class="footer-info-email" href="mailto:a.dragunov@tdaliance.ru">a.dragunov@tdaliance.ru</a>
+          </div>
+          <!-- /.mobile-info -->
+    
+          <div class="footer-info-social">
+            <a class="footer-info-vk" href="#">
+              <svg class="phone-icon" width="24" height="24">
+                <use href="img/sprite.svg#vk"></use>
+              </svg>
+            </a>
+            <a class="footer-info-inst" href="#">
+              <svg class="phone-icon" width="24" height="24">
+                <use href="img/sprite.svg#inst"></use>
+              </svg>
+            </a>
+          </div>
+           <!-- /.footer-info-social -->
+        </div>
+        
+  </div>
+  <hr color="#ebebf0" class="footer-seporator">
+  <div class="container">
+        <div class="footer-bottom">
+          <div class="footer-menu-wrapper">
+            <h2 class="footer-menu-title">Контрактное производство</h2>
+            <ul class="footer-menu-list footer-menu-column">
+              <li class="footer-menu-item">
+                <a href="autohim.html" class="footer-menu-link">Автомобильная химия</a>
+              </li>
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Бытовая химия</a>
+              </li>
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Дезинфицирующие средства</a>
+              </li>
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Пищевые аэрозоли</a>
+              </li>
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Косметическая продукция</a>
+              </li>
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Краски аэрозольные</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="footer-menu-wrapper">
+            <h2 class="footer-menu-title">Собственные марки</h2>
+            <ul class="footer-menu-list">
+              <li class="footer-menu-item">
+                <a href="ag-tect.html" class="footer-menu-link">Автохимия AG-Tech</a>
+              </li>
+              <li class="footer-menu-item">
+                <a href="#" class="footer-menu-link">Автохимия AP</a>
+              </li>
+          </div>
+
+        <div class="footer-menu-wrapper">
+          <ul class="footer-menu-list">
+            <li class="footer-menu-item">
+              <a href="about.html" class="footer-menu-link footer-menu-link-bold">О компании</a>
+            </li>
+            <li class="footer-menu-item">
+              <a href="news.html" class="footer-menu-link footer-menu-link-bold">Новости</a>
+            </li>
+            <li class="footer-menu-item">
+              <a href="contacts.html" class="footer-menu-link footer-menu-link-bold">Контакты</a>
+            </li>
+        </div>
+        
+        </div>
+  </div>
+  <hr color="#ebebf0" class="footer-seporator footer-seporator-2">
+  <div class="container">
+    <div class="footer-wrapper">
+      <div class="footer-legal">
+        <p class="footer-copyright">&copy; <?php echo date('Y');?> «Aliance Production». Все права защищены.</p>
+        <a href="policy.php" class="footer-policy">Политики конфиденциальности</a>
+      </div>
+      <div class="footer-author">
+        <span class="made-in">Сделано в</span>
+        <span class="stupid-designer">Дизайн и разработка:</span>
+        <svg class="ruso-logo" width="24" height="24">
+          <use href="img/sprite.svg#ruso"></use>
+        </svg>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- /.footer -->
+
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/just-validate.production.min.js"></script>
+<script src="js/main.js" ></script>
+
+</body>
+</html>

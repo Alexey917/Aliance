e1840d589bfa6207923d71201e4c7a68a92cb1a5
Alexey Ageev <pochtovii.1996@mail.ru>
2022-10-25 01:35:13 +0300

    fix: pixel perfect and adaptive

--- marks.php
+++ marks.php
@@ -2,6 +2,7 @@
 $page_title = 'Собственные торговые марки';
 $link_breadcrumb = 'marks.php';
 $section_class_trademarks = 'trademarks-section';
+$page_title_contract_production_mobile = 'contract-production-mobile';
 include_once('header-page.php');?>
 
 <?php include_once('template-parts/trademarks-block.php');?>
